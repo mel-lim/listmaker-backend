ad21804d4b97a9854601ee6d09a8cd8e4787b30e
mel <43204513+mel-lim@users.noreply.github.com>
2021-04-23 18:00:43 -0700

    add test endpoint which doesnt require authentication

--- api/api.js
+++ api/api.js
@@ -8,6 +8,21 @@ apiRouter.get('', (request, response, next) => {
     response.json({ info: 'Node.js, Express, and Postgres API' });
 });
 
+// Centralise our data access for reuseability per the node-postgres library guide
+const db = require('../db');
+
+apiRouter.get('/test', (request, response, next) => {
+    db.query('SELECT * FROM trip', [], (error, results) => {
+
+        if (error) {
+            next(error);
+        }
+
+        response.status(200).json({ "trips": results.rows });
+
+
+})
+
 const authRouter = require('../routes/auth');
 apiRouter.use('/appusers', authRouter);
  
