43842fa3af9f7bb6864f3e262a32af794b810b9e
mel <43204513+mel-lim@users.noreply.github.com>
2021-04-30 20:30:46 -0700

    add cproduction config for deployment to heroku and logic to detect whether in production or dev

--- db/index.js
+++ db/index.js
@@ -1,30 +1,30 @@
 // CENTRALISED DATABASE INTERACTIONS FILE
 // All of our db interactions will come through here
 
-const Pool = require('pg').Pool
+const { Pool } = require('pg');
 const dotenv = require('dotenv');
-
 dotenv.config();
 
-const pool = new Pool({
-    /* connectionString: process.env.DATABASE_URL, 
-    ssl: true, */
+const devConfig = {
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
     password: process.env.DB_PASS,
     port: process.env.DB_PORT
-});
+};
+
+const productionConfig = {
+    connectionString: process.env.DATABASE_URL, // This will come from our Heroku addon
+    ssl: {
+        rejectUnauthorized: false
+    }
+}
+
+const pool = new Pool(
+    process.env.NODE_ENV === "production" ? productionConfig : devConfig
+);
 
 module.exports = {
-    /* query: (text, params, callback) => {
-        return pool.query(text, params, callback)
-    }, */
     query: (text, params) => pool.query(text, params),
-    /* getClient: (callback) => {
-        pool.connect((err, client, done) => {
-            callback(err, client, done);
-        })
-    } */
     getClient: () => pool.connect()
 };
