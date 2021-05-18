21677fe963fd9a903c36203f0fa1bd51d9af2bcf
mel <43204513+mel-lim@users.noreply.github.com>
2021-05-17 17:00:03 -0700

    add navigation device as an item in ski-tour

--- db/migration2.sql
+++ db/migration2.sql
@@ -6,3 +6,6 @@ ADD COLUMN is_guest BOOLEAN DEFAULT false;
 
 ALTER TABLE app_user
 ADD COLUMN is_admin BOOLEAN DEFAULT false;
+
+INSERT INTO template_list_item (name, list_id)
+VALUES ('Navigation - phone, GPS device, map/compass', 1);
